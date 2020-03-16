dd5f6cf006c788b3a86755a8b82dbc8b28dc42d5
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-03-15 23:50:36 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,24 @@
+window.addEventListener('load', init);
 
+function init() {
+  let $ = function (id) {
+    'use strict';
+    return window.document.getElementById(id);
+  };
+  let allButtons = $('allButtons');
+  let result = $('result');
+  let equal = $('equal');
+  
+  allButtons.addEventListener('click', enter); 
+  function enter(num) {
+    window.console.log(num.target.value);
+    $(result).value += num.target.value;
+  }
+  function calculate(num) {
+    $('result').value += num.target.value;
+    console.log(num.target.value);
+  }
+  equal.addEventListener('click', function calculate() {
+    $('result').value = evaluate($('result').value);
+  })
+}
